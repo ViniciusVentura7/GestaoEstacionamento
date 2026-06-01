54837d8558f6363e57c98388cda3ba162d900791
vinicius <viniven7@gmail.com>
2026-06-01 20:40:01 -0300

    :heavy_plus_sign: build: instalando dependências

---
+++ src/main/java/br/com/estudo/GestaoEstacionamento/GestaoEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.GestaoEstacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudo/GestaoEstacionamento/GestaoEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.GestaoEstacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
